35effbb5ad6e78a66fbee4585bfa50b881890741
Awa Assahna <awadesmoline@gmail.com>
2018-08-31 12:16:12 +0100

    local install

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -9,8 +9,13 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname}/../../db/config.json`)[env];
 
 const db = {};
+let sequelize = null;
 
-const sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
